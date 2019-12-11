bec812076a76aff8f56177c608d444bfa40b84e3
Kayla Wood <kaylaewood@gmail.com>
2019-12-11 09:45:16 -0700

    Create Form component

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReservationContainer from '../ReservationContainer/ReservationContainer'
+import ReservationContainer from '../ReservationContainer/ReservationContainer';
+import Form from '../Form/Form'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'></div>
+        <div className='resy-form'>
+          <Form />
+        </div>
         <div className='resy-container'>
           <ReservationContainer />
         </div>
---
+++ src/Form/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <p>FORM</p>
+    )
+  }
+}
+
+export default Form;

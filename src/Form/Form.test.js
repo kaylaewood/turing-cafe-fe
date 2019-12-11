0ff5fceb63bcbeb5dfc22bd23372b56e02af3577
Kayla Wood <kaylaewood@gmail.com>
2019-12-11 10:44:07 -0700

    Test methods in Form

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -4,12 +4,33 @@ import Form from './Form';
 
 describe('Form', () => {
   let submitReservationMock = jest.fn();
+  let wrapper;
+
+  beforeEach(() => {
+      wrapper = shallow(<Form
+      submitReservation={submitReservationMock}
+      />)
+  })
 
   it('should match snapshot as expected', () => {
-    const wrapper = shallow(<Form
-      submitReservation={submitReservationMock}
-    />)
 
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should update state when handleChange is invoked', () => {
+    const event = {
+      target: {
+        name: 'name',
+        value: 'Robbie'
+      }
+    }
+
+    wrapper.instance().handleChange(event);
+    expect(wrapper.state('name')).toEqual('Robbie');
+  })
+
+  it('should invoke submitReservation when button is clicked', () => {
+    wrapper.find('button').simulate('click');
+    expect(submitReservationMock).toHaveBeenCalled();
   })
 })

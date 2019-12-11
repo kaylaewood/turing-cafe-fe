0c504231862146df3433ac593604ed8710880208
Kayla Wood <kaylaewood@gmail.com>
2019-12-11 11:17:39 -0700

    Test that cancel button invokes function;

--- src/ReservationCard/ReservationCard.test.js
+++ src/ReservationCard/ReservationCard.test.js
@@ -3,15 +3,26 @@ import { shallow } from 'enzyme';
 import ReservationCard from './ReservationCard';
 
 describe('ReservationCard', () => {
-  it('should match snapshot as expected', () => {
-    const wrapper = shallow(<ReservationCard
+  let wrapper;
+  let cancelReservationMock = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(<ReservationCard
       id='1'
       name='Kayla'
       date='12/03'
       time='2:45'
       number='3'
+      cancelReservation={cancelReservationMock}
     />)
+  })
 
+  it('should match snapshot as expected', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should invoke cancelReservation when button is clicked', () => {
+    wrapper.find('button').simulate('click');
+    expect(cancelReservationMock).toHaveBeenCalled();
   })
 })

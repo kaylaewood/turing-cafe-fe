153c6ac9988a6c4117116b997a624eee806b7894
Kayla Wood <kaylaewood@gmail.com>
2019-12-11 09:53:17 -0700

    Create form skeleton with styling

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,11 +4,45 @@ import './Form.css';
 class Form extends Component {
   constructor() {
     super()
+    this.state = {
+      reservation: {
+        name: '',
+        date: '',
+        time: '',
+        number: null
+      }
+    }
   }
 
   render() {
     return (
-      <p>FORM</p>
+      <form>
+        <input
+          type='text'
+          name='name'
+          value={this.state.reservation.name}
+          placeholder='Name'
+        />
+        <input
+          type='text'
+          name='date'
+          value={this.state.reservation.date}
+          placeholder='Date (mm/dd)'
+        />
+        <input
+          type='text'
+          name='time'
+          value={this.state.reservation.time}
+          placeholder='Time'
+        />
+        <input
+          type='text'
+          name='number'
+          value={this.state.reservation.number}
+          placeholder='Number of Guests'
+        />
+        <button className='reserve-button'>Make Reservation</button>
+      </form>
     )
   }
 }
